a9d2f43190dfd3063412fe271c1e4b9276283715
Menn <m@seedwebs.com>
2024-04-10 20:38:04 +0700

    fix error 'tips'

--- src/utils/data-utils.ts
+++ src/utils/data-utils.ts
@@ -1,25 +1,25 @@
 import { type CollectionEntry } from 'astro:content';
 import { slugify } from './common-utils';
 
-export function sortItemsByDateDesc(itemA: CollectionEntry<'stories' | 'tips'>, itemB: CollectionEntry<'stories' | 'tips'>) {
-    return new Date(itemB.data.publishDate).getTime() - new Date(itemA.data.publishDate).getTime();
+export function sortItemsByDateDesc(itemA: CollectionEntry<'stories'>, itemB: CollectionEntry<'stories'>) {
+  return new Date(itemB.data.publishDate).getTime() - new Date(itemA.data.publishDate).getTime();
 }
 
 export function getAllTags(posts: CollectionEntry<'stories'>[]) {
-    const tags: string[] = [...new Set(posts.flatMap((post) => post.data.tags || []).filter(Boolean))];
-    return tags
-        .map((tag) => {
-            return {
-                name: tag,
-                slug: slugify(tag)
-            };
-        })
-        .filter((obj, pos, arr) => {
-            return arr.map((mapObj) => mapObj.slug).indexOf(obj.slug) === pos;
-        });
+  const tags: string[] = [...new Set(posts.flatMap((post) => post.data.tags || []).filter(Boolean))];
+  return tags
+    .map((tag) => {
+      return {
+        name: tag,
+        slug: slugify(tag)
+      };
+    })
+    .filter((obj, pos, arr) => {
+      return arr.map((mapObj) => mapObj.slug).indexOf(obj.slug) === pos;
+    });
 }
 
 export function getPostsByTag(posts: CollectionEntry<'stories'>[], tagSlug: string) {
-    const filteredPosts: CollectionEntry<'stories'>[] = posts.filter((post) => (post.data.tags || []).map((tag) => slugify(tag)).includes(tagSlug));
-    return filteredPosts;
+  const filteredPosts: CollectionEntry<'stories'>[] = posts.filter((post) => (post.data.tags || []).map((tag) => slugify(tag)).includes(tagSlug));
+  return filteredPosts;
 }
