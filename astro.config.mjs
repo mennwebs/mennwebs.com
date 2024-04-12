361ecf980f715f6c6b7980c5f2c872cf87a35174
Menn <m@seedwebs.com>
2024-04-12 15:45:11 +0700

    add inline css

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-
 import icon from 'astro-icon';
+
+import playformInline from "@playform/inline";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mennwebs.com/',
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind({
-      applyBaseStyles: false
-    }),
-    icon()
-  ]
+  integrations: [mdx(), sitemap(), tailwind({
+    applyBaseStyles: false
+  }), icon(), playformInline()]
 });
